57b98074374d6660f08a429be96bace0927addfb
powlpy <powlpy@gmail.com>
2013-12-10 14:29:02 +0100

    Update accesBDD.php

--- base/accesBDD.php
+++ base/accesBDD.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 $hostname='infolimon'; //infolimon
 $login='bexchauveto'; //login iut
 $passwd='1105017183J'; // votre passwd
@@ -6,4 +7,3 @@ $base='bexchauveto'; //login iut
 $connect = mysql_connect($hostname,$login,$passwd) or die ("erreur de connexion");
 mysql_select_db($base,$connect) or die ("erreur de connexion base");
 ?>
-
